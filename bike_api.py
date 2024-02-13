d742bd315549a11bfae5ff263bd2bcc9a46cda9e
Adam Southwick <adamsouthwick100@gmail.com>
2024-02-13 11:40:37 +0000

    updated bike gettdata function to return python dictionary

--- bike_api.py
+++ bike_api.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+from ast import literal_eval
 
 def load_bike_data():
     load_dotenv()
@@ -11,3 +12,19 @@ def load_bike_data():
         print(api_request.reason)
         return
     return api_request
+
+def relavent_bike_dict():
+    data = load_bike_data()
+    j_data = data.json()
+    for d in j_data:
+        #ALL RELAVENT DATA
+        id = d['number']
+        lat = d['position']['lat']
+        lng = d['position']['lat']
+        av_bike_stands = d['available_bike_stands']
+        av_bikes = d['available_bikes']
+        status = d['status']
+        name = d['adress']
+        timestamp = d['last_update']
+
+relavent_bike_dict()
