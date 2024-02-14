5da779ec474811a807c148801e7b864b53f94ba8
Adam Southwick <adamsouthwick100@gmail.com>
2024-02-14 19:48:04 +0000

    cleaned up the apis

--- bike_api.py
+++ bike_api.py
@@ -1,9 +1,8 @@
 import os
 import requests
 from dotenv import load_dotenv
-from ast import literal_eval
 
-def request_bike_data():
+def api_call():
     load_dotenv()
     api_key = os.getenv('bike_key')
     contract = 'Dublin'
@@ -14,17 +13,25 @@ def request_bike_data():
     return api_request
 
 def relavent_bike_dict():
-    data = request_bike_data()
-    j_data = data.json()
-    for d in j_data:
+    data = api_call()
+    data_json = data.json()
+    for d in data_json:
         #ALL RELAVENT DATA
         id = d['number']
         lat = d['position']['lat']
-        lng = d['position']['lat']
+        lng = d['position']['lng']
         av_bike_stands = d['available_bike_stands']
         av_bikes = d['available_bikes']
         status = d['status']
-        name = d['adress']
+        name = d['address']
         timestamp = d['last_update']
+        print(id)
+        print(lat)
+        print(lng)
+        print(av_bike_stands)
+        print(av_bikes)
+        print(status)
+        print(name)
+        print(timestamp)
 
 relavent_bike_dict()
--- weather_api.py
+++ weather_api.py
@@ -3,23 +3,25 @@ import openmeteo_requests
 # Setup the Open-Meteo API client
 openmeteo = openmeteo_requests.Client()
 
-# Make sure all required weather variables are listed here
-url = "https://api.open-meteo.com/v1/forecast"
-params = {
-	"latitude": 53.35, #Dublin lat
-	"longitude": 6.26, #Dublin lng
-	"current": ["temperature_2m", "precipitation", "wind_speed_10m"]
-}
-api_request = openmeteo.weather_api(url, params=params)
-response = api_request[0]
+def api_call():
+	url = "https://api.open-meteo.com/v1/forecast"
+	params = {
+		"latitude": 53.35, #Dublin lat
+		"longitude": 6.26, #Dublin lng
+		"current": ["temperature_2m", "precipitation", "wind_speed_10m"]
+	}
+	api_request = openmeteo.weather_api(url, params=params)
+	return api_request[0].Current()
 
-# Current values
-current = response.Current()
-current_temperature = current.Variables(0).Value()
-current_precipitation = current.Variables(1).Value()
-current_wind_speed = current.Variables(2).Value()
+def current_weather():
+	current = api_call()
+	current_temperature = current.Variables(0).Value()
+	current_precipitation = current.Variables(1).Value()
+	current_wind_speed = current.Variables(2).Value()
+ 
+	print(f"Current time {current.Time()}")
+	print(f"Current temperature {current_temperature}")
+	print(f"Current precipitation {current_precipitation}")
+	print(f"Current wind_speed {current_wind_speed}")
 
-print(f"Current time {current.Time()}")
-print(f"Current temperature {current_temperature}")
-print(f"Current precipitation {current_precipitation}")
-print(f"Current wind_speed {current_wind_speed}")
+current_weather()
