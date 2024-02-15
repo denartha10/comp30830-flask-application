1f0d733c6119c3c1e0588f37bc55b08b541d9903
Adam Southwick <adamsouthwick100@gmail.com>
2024-02-15 17:38:57 +0000

    finalized bike api script function, and made a database manager script that populates the database with bike availability data

--- bike_api.py
+++ bike_api.py
@@ -10,28 +10,5 @@ def api_call():
     if api_request.status_code not in range(200, 299):
         print(api_request.reason)
         return
-    return api_request
-
-def relavent_bike_dict():
-    data = api_call()
-    data_json = data.json()
-    for d in data_json:
-        #ALL RELAVENT DATA
-        id = d['number']
-        lat = d['position']['lat']
-        lng = d['position']['lng']
-        av_bike_stands = d['available_bike_stands']
-        av_bikes = d['available_bikes']
-        status = d['status']
-        name = d['address']
-        timestamp = d['last_update']
-        print(id)
-        print(lat)
-        print(lng)
-        print(av_bike_stands)
-        print(av_bikes)
-        print(status)
-        print(name)
-        print(timestamp)
-
-relavent_bike_dict()
+    data_json = api_request.json()
+    return data_json
--- db_manager.py
+++ db_manager.py
@@ -1,16 +1,47 @@
 import os
 from dotenv import load_dotenv
-import sqlalchemy as db
-import pandas as pd
+
+from sqlalchemy import create_engine, Column, Integer, BIGINT, Float, String, MetaData
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+import bike_api as ba
+
+Base = declarative_base()
+
+class Availability(Base):
+    __tablename__ = 'availability'
+    id = Column(Integer, primary_key=True)
+    last_update = Column(BIGINT, primary_key=True)
+    available_bikes = Column(Integer)
+    available_stands = Column(Integer)
+    status = Column(String)
+
+    def __init__(self, id, up, bikes, stands, status):
+        self.id = id
+        self.last_update = up
+        self.available_bikes = bikes
+        self.available_stands = stands
+        self.status = status
 
 def engine_params():
     load_dotenv()
     user = 'Adam'
     password = os.getenv('rds_pswrd')
-    host = '172.31.16.0/20'
+    host = 'dublinbikesdb.cvy4qywa206q.eu-west-1.rds.amazonaws.com'
     port = '3306'
-    db_name = 'dublinbikesdb'
-    return f'mysql://{user}:{password}@{host}:{port}/{db_name}'
+    db_name = 'Dublin_Bikes'
+    return f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db_name}'
 
-#default engine
-engine = db.create_engine(engine_params())
+engine = create_engine(engine_params())
+metadata = MetaData()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Add new rows
+for d in ba.api_call():
+    new_av = Availability(id = d['number'], up = d['last_update'], bikes = d['available_bikes'], stands = d['available_bike_stands'], status = d['status'])
+    session.add(new_av)
+
+session.commit()
+session.close()
