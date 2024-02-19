bece384c295e33a5a225b1d560b21a1c22199010
Adam Southwick <adamsouthwick100@gmail.com>
2024-02-19 16:15:29 +0000

    added db name

--- bike_api.py
+++ bike_api.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, BIGINT, String, MetaData
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+from datetime import datetime, time
+import pytz
+
 Base = declarative_base()
 
 class Availability(Base):
@@ -38,19 +41,29 @@ def engine_params():
     password = 'kukfiv-zubsyd-1Pejpu'
     host = 'database-1.c38umsk2i6vi.eu-north-1.rds.amazonaws.com'
     port = '3306'
-    db_name = ''
+    db_name = 'bike_db'
     return f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db_name}'
 
-engine = create_engine(engine_params())
-metadata = MetaData()
+def push_to_db():
+    engine = create_engine(engine_params())
+    metadata = MetaData()
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# Add new rows
-for d in api_call():
-    new_av = Availability(id = d['number'], up = d['last_update'], bikes = d['available_bikes'], stands = d['available_bike_stands'], status = d['status'])
-    session.add(new_av)
+    # Add new rows
+    for d in api_call():
+        new_av = Availability(id = d['number'], up = d['last_update'], bikes = d['available_bikes'], stands = d['available_bike_stands'], status = d['status'])
+        session.add(new_av)
 
-session.commit()
-session.close()
+    session.commit()
+    session.close()
+
+def is_valid_time(now):
+    return time(0, 30) <= now <= time(5, 0)
+
+dublin_timezone = pytz.timezone('Europe/Dublin') 
+current_time_dublin = datetime.now(dublin_timezone)
+
+if is_valid_time(current_time_dublin.time()):
+    push_to_db()
--- single_use_scripts/station_filler.py
+++ single_use_scripts/station_filler.py
@@ -34,7 +34,7 @@ def engine_params():
     password = 'kukfiv-zubsyd-1Pejpu'
     host = 'database-1.c38umsk2i6vi.eu-north-1.rds.amazonaws.com'
     port = '3306'
-    db_name = ''
+    db_name = 'bike_db'
     return f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db_name}'
 
 engine = create_engine(engine_params())
