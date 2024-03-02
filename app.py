547f79f9bf9635bc031f8b9ea4e709149a03246a
Adam Southwick <adamsouthwick100@gmail.com>
2024-03-02 11:06:27 +0000

    reduced engine connections to 1 and combined the data pull, also made stations clickable under th new system

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_caching import Cache
 import pandas as pd
 from sqlalchemy import create_engine
-import json
 
 config = {
     "DEBUG": True,          # some Flask specific configs
@@ -14,7 +13,7 @@ app = Flask(__name__, static_url_path='')
 app.config.from_mapping(config)
 cache = Cache(app)
 
-def engine_params():
+def engine_paramss():
     user = 'admin'
     password = 'kukfiv-zubsyd-1Pejpu'
     host = 'database-1.c38umsk2i6vi.eu-north-1.rds.amazonaws.com'
@@ -22,44 +21,36 @@ def engine_params():
     db_name = 'bike_db'
     return f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db_name}'
 
+#--------------------------- DELETE BEFORE PUSHING
+def engine_params():
+    user = 'root'
+    password = 'Onion100$$'
+    host = 'localhost'
+    port = '3306'
+    db_name = 'alchemy_test'
+    return f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db_name}'
+#---------------------------
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-
-@app.route('/stations')
-def get_stations():
-    engine = create_engine(engine_params())
-    with engine.connect() as conn, conn.begin():
-        data = pd.read_sql_table("stations", conn)
-        conn.close()
-        data_t = data.transpose()
-        return data_t.to_json()
-
-@app.route('/weather')
-def get_weather():
-    engine = create_engine(engine_params())
-    with engine.connect() as conn, conn.begin():
-        data = pd.read_sql_table("weather", conn)
-        conn.close()
-        data_t = data.transpose()
-        return data_t.to_json()
-    
-    #NOT LIVE CURRENTLY PULLING 
-@app.route('/test')
-def getttt_weather():
+ 
+@app.route('/data')
+def pull_data():
     engine = create_engine(engine_params())
     with engine.connect() as conn, conn.begin():
         weather = pd.read_sql_table("weather", conn)
+        #only use the most recent weather entry bc thats current
+        weather_last = weather.tail(1)
         bike_data = pd.read_sql_table("stations", conn)
         conn.close()
         bike_data_t = bike_data.transpose()
-        weather_t = weather.transpose()
+        weather_t = weather_last.transpose()
         dict = {
             'bike' : bike_data_t.to_json(),
             'weather' : weather_t.to_json()
         }
-        return json.dumps(dict)
+        return jsonify(dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -1,101 +1,101 @@
 // Event listener for when the DOM content is fully loaded
-document.addEventListener("DOMContentLoaded", () => {
-  // Mapping HTML IDs to elements
-  const elements = {
-    main: document.getElementById("mainContent"),
-    menu: document.getElementById("sidebar"),
-    menuToggle: document.getElementById("sidebarToggle"),
-    weatherMenu: document.getElementById("weatherMenu"),
-    weatherToggle: document.getElementById("weatherToggle"),
-    closeWeather: document.getElementById("closeWeather"),
-    searchBar: document.getElementById("searchBar"),
-    searchTrip: document.getElementById("searchTrip"),
-  };
+document.addEventListener('DOMContentLoaded', () => {
+	// Mapping HTML IDs to elements
+	const elements = {
+		main: document.getElementById('mainContent'),
+		menu: document.getElementById('sidebar'),
+		menuToggle: document.getElementById('sidebarToggle'),
+		weatherMenu: document.getElementById('weatherMenu'),
+		weatherToggle: document.getElementById('weatherToggle'),
+		closeWeather: document.getElementById('closeWeather'),
+		searchBar: document.getElementById('searchBar'),
+		searchTrip: document.getElementById('searchTrip'),
+	};
 
-  // Toggle sidebar visibility when the sidebar toggle button is clicked
-  elements.menuToggle.addEventListener("click", () => {
-    elements.menu.classList.toggle("-translate-x-96");
-    elements.main.classList.toggle("translate-x-96");
-  });
+	// Toggle sidebar visibility when the sidebar toggle button is clicked
+	elements.menuToggle.addEventListener('click', () => {
+		elements.menu.classList.toggle('-translate-x-96');
+		elements.main.classList.toggle('translate-x-96');
+	});
 
-  // Clear the search bar when clicked
-  elements.searchBar.addEventListener("click", () => {
-    elements.searchBar.value = "";
-  });
+	// Clear the search bar when clicked
+	elements.searchBar.addEventListener('click', () => {
+		elements.searchBar.value = '';
+	});
 
-  // Toggle visibility of the weather menu when weather toggle button is clicked
-  elements.weatherToggle?.addEventListener("click", () =>
-    toggleElementVisibility(elements.weatherMenu),
-  );
+	// Toggle visibility of the weather menu when weather toggle button is clicked
+	elements.weatherToggle?.addEventListener('click', () =>
+		toggleElementVisibility(elements.weatherMenu)
+	);
 
-  // Hide the weather menu when the close button is clicked
-  elements.closeWeather?.addEventListener("click", () =>
-    elements.weatherMenu.classList.add("hidden"),
-  );
+	// Hide the weather menu when the close button is clicked
+	elements.closeWeather?.addEventListener('click', () =>
+		elements.weatherMenu.classList.add('hidden')
+	);
 
-  // Call the displayClock function to display the current time
-  displayClock();
+	// Call the displayClock function to display the current time
+	displayClock();
 });
 
 // Function to toggle visibility of an element
 const toggleElementVisibility = (element) => {
-  element?.classList.toggle("hidden");
+	element?.classList.toggle('hidden');
 };
 
 // Function to display the current time
 const displayClock = () => {
-  // Update the clock every second
-  const updateClock = () => {
-    const currentTime = new Date();
-    const hours = currentTime.getHours();
-    const minutes = currentTime.getMinutes();
-    const display = `${hours}:${minutes}`;
-    document.getElementById("liveTimer").textContent = display;
-    setTimeout(updateClock, 1000);
-  };
-  // Initial call to update the clock
-  updateClock();
+	// Update the clock every second
+	const updateClock = () => {
+		const currentTime = new Date();
+		const hours = currentTime.getHours();
+		const minutes = currentTime.getMinutes();
+		const display = `${hours}:${minutes}`;
+		document.getElementById('liveTimer').textContent = display;
+		setTimeout(updateClock, 1000);
+	};
+	// Initial call to update the clock
+	updateClock();
 };
 
 // Function to clear inputs when the menu is closed
 const clearInput = () => {
-  ["searchBar", "startingDestination", "endDestination"].forEach((id) => {
-    const element = document.getElementById(id);
-    // If element exists and menu is hidden, sets value of the element to an empty string
-    if (
-      element &&
-      document.getElementById("menu").classList.contains("hidden")
-    ) {
-      element.value = "";
-    }
-  });
+	['searchBar', 'startingDestination', 'endDestination'].forEach((id) => {
+		const element = document.getElementById(id);
+		// If element exists and menu is hidden, sets value of the element to an empty string
+		if (
+			element &&
+			document.getElementById('menu').classList.contains('hidden')
+		) {
+			element.value = '';
+		}
+	});
 };
 
 // Placeholder for the actual search functionality - To be completed
 const performSearch = () => {
-  const searchBar = document.getElementById("searchBar");
-  const searchText = searchBar.value.trim();
+	const searchBar = document.getElementById('searchBar');
+	const searchText = searchBar.value.trim();
 };
 
 // Initialization function
 function init() {
-  pullBikeData();
-  pullWeatherData();
+	pullData();
 }
 
 // Function to fetch bike data from the server
-const pullBikeData = async () => {
-  try {
-    const response = await fetch("/stations");
-    if (!response.ok) {
-      throw new Error("Error");
-    }
-    const data = await response.json();
-    initMap(data);
-  } catch (error) {
-    console.error(error);
-    alert(error);
-  }
+const pullData = async () => {
+	try {
+		const response = await fetch('/data');
+		if (!response.ok) {
+			throw new Error('Error');
+		}
+		const data = await response.json();
+		initMap(JSON.parse(data.bike));
+		populateWeather(JSON.parse(data.weather));
+	} catch (error) {
+		console.error(error);
+		alert(error);
+	}
 };
 
 // Declaration of variables related to the map
@@ -104,138 +104,132 @@ let markersArr = [];
 
 // Function to initialize the map with the received data
 const initMap = async (data) => {
-  // Import necessary libraries
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement, PinElement } =
-    await google.maps.importLibrary("marker");
+	// Import necessary libraries
+	const {Map} = await google.maps.importLibrary('maps');
+	const {AdvancedMarkerElement, PinElement} = await google.maps.importLibrary(
+		'marker'
+	);
 
-  // Initialize the map
-  map = new Map(document.getElementById("map"), {
-    zoom: 14,
-    center: { lat: 53.347, lng: -6.27 },
-    mapTypeControl: false,
-    streetViewControl: false,
-    ClickableIcons: false,
-    mapId: "dublin_bike_map_id",
-  });
+	// Initialize the map
+	map = new Map(document.getElementById('map'), {
+		zoom: 14,
+		center: {lat: 53.347, lng: -6.27},
+		mapTypeControl: false,
+		streetViewControl: false,
+		ClickableIcons: false,
+		mapId: 'dublin_bike_map_id',
+	});
 
-  // Iterate through data and populate the markers
-  for (let dat in data) {
-    let d = data[dat];
-    let background;
-    let border;
+	// Iterate through data and populate the markers
+	for (let dat in data) {
+		let d = data[dat];
+		let background;
+		let border;
 
-    // Determine pin color based on bikes available and status
-    if (d.status != "OPEN") {
-      background = "#00000000";
-      border = "#00000000";
-      //more than 5 bikes = green
-    } else if (d.available_bikes > 5) {
-      background = "#2ED18A";
-      border = "#137333";
-      //5-3 bikes = yellow
-    } else if (d.available_bikes > 2) {
-      background = "#FFFF00";
-      border = "#B3B300";
-      //1-2 bikes = orange
-    } else if (d.available_bikes > 0) {
-      background = "#FF9900";
-      border = "#995C00";
-      //no bikes = red
-    } else {
-      background = "#D22D2F";
-      border = "#6E1718";
-    }
+		// Determine pin color based on bikes available and status
+		if (d.status != 'OPEN') {
+			background = '#00000000';
+			border = '#00000000';
+			//more than 5 bikes = green
+		} else if (d.available_bikes > 5) {
+			background = '#2ED18A';
+			border = '#137333';
+			//5-3 bikes = yellow
+		} else if (d.available_bikes > 2) {
+			background = '#FFFF00';
+			border = '#B3B300';
+			//1-2 bikes = orange
+		} else if (d.available_bikes > 0) {
+			background = '#FF9900';
+			border = '#995C00';
+			//no bikes = red
+		} else {
+			background = '#D22D2F';
+			border = '#6E1718';
+		}
 
-    info_string = `
+		info_string = `
       <div class="max-w-xs">
         <div class="bg-white p-4 rounded-lg shadow-lg">
             <h2 class="text-lg font-semibold mb-2">Bike Station Information</h2>
             <div class="flex flex-col space-y-1">
                 <div><span class="font-semibold">ID:</span> <span class="text-gray-600">${
-                  d.id
-                }</span></div>
+									d.id
+								}</span></div>
                 <div><span class="font-semibold">Name:</span> <span class="text-gray-600">${
-                  d.name
-                }</span></div>
+									d.name
+								}</span></div>
                 <div><span class="font-semibold">Latitude:</span> <span class="text-gray-600">${
-                  d.lat
-                }</span></div>
+									d.lat
+								}</span></div>
                 <div><span class="font-semibold">Longitude:</span> <span class="text-gray-600">${
-                  d.lng
-                }</span></div>
+									d.lng
+								}</span></div>
                 <div><span class="font-semibold">Bikes Available:</span> <span class="${
-                  d.bikes_open === 0 ? "text-red-600" : "text-gray-600"
-                }">${d.bikes_open}</span></div>
+									d.bikes_open === 0 ? 'text-red-600' : 'text-gray-600'
+								}">${d.bikes_open}</span></div>
                 <div><span class="font-semibold">Stands Available:</span> <span class="text-gray-600">${
-                  d.stands_open
-                }</span></div>
+									d.stands_open
+								}</span></div>
                 <div><span class="font-semibold">Status:</span> <span class="${
-                  d.status === "OPEN" ? "text-green-600" : "text-red-600"
-                }">${d.status}</span></div>
+									d.status === 'OPEN' ? 'text-green-600' : 'text-red-600'
+								}">${d.status}</span></div>
             </div>
         </div>
       </div>`;
 
-    // Create pin style
-    let pinStyle = new PinElement({
-      background: background,
-      borderColor: border,
-      glyphColor: border,
-    });
+		// Create pin style
+		let pinStyle = new PinElement({
+			background: background,
+			borderColor: border,
+			glyphColor: border,
+		});
 
-    const infowindow = new google.maps.InfoWindow({
-      content: info_string,
-      ariaLabel: `Bikes ${d.id.toString()}`,
-    });
+		const infowindow = new google.maps.InfoWindow({
+			content: info_string,
+			ariaLabel: `Bikes ${d.id.toString()}`,
+		});
 
-    // Create marker
-    let newMarker = new AdvancedMarkerElement({
-      map: map,
-      position: { lat: d.lat, lng: d.lng },
-      title: d.id.toString(),
-      content: pinStyle.element,
-    });
+		// Create marker
+		let newMarker = new AdvancedMarkerElement({
+			map: map,
+			position: {lat: d.lat, lng: d.lng},
+			title: d.id.toString(),
+			content: pinStyle.element,
+			gmpClickable: true,
+		});
 
-    //Add event listener not available for the AdvancedMarkerElement in the version of the API we are using
-    //We can use Marker element instead of AdvancedMarkerElement since that is the version of the API we are using
-    newMarker.addEventListener("mouseover", () => {
-      infowindow.open({
-        anchor: newMarker,
-        map,
-      });
-    });
+		//Add event listener not available for the AdvancedMarkerElement in the version of the API we are using
+		//We can use Marker element instead of AdvancedMarkerElement since that is the version of the API we are using
+		newMarker.addEventListener('mouseover', () => {
+			infowindow.open({
+				anchor: newMarker,
+				map,
+			});
+		});
 
-    newMarker.addEventListener("mouseout", () => {
-      infowindow.close();
-    });
-    // Add marker to the array
-    markersArr.push(newMarker);
-  }
-};
-// Function called when a marker icon is clicked
-const iconClick = (stationInfo) => {
-  console.log(stationInfo);
-};
+		newMarker.addEventListener('mouseout', () => {
+			infowindow.close();
+		});
 
-// Function to fetch weather data from the server
-const pullWeatherData = async () => {
-  try {
-    const response = await fetch("/weather");
-    if (!response.ok) {
-      throw new Error("Error");
-    }
-    const data = await response.json();
-    populateWeather(data);
-  } catch (error) {
-    console.error(error);
-    alert(error);
-  }
+		newMarker.addEventListener('gmp-click', () => {
+			console.log(d);
+		});
+
+		// Add marker to the array
+		markersArr.push(newMarker);
+	}
 };
 
 // Function to populate weather data on the front end
 const populateWeather = (data) => {
-  document.getElementById("tempNum").innerText = data["0"]["temperature"];
-  document.getElementById("precNum").innerText = data["0"]["precipitation"];
-  document.getElementById("windNum").innerText = data["0"]["wind"];
+  //we dont know what the key will be bc its the last row number in db
+  //so we have to use a for loop that only runs once
+	for (let dat in data) {
+		let d = data[dat];
+		console.log(d);
+		document.getElementById('tempNum').innerText = d['temperature'];
+		document.getElementById('precNum').innerText = d['precipitation'];
+		document.getElementById('windNum').innerText = d['wind'];
+	}
 };
