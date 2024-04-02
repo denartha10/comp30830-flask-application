78c61b0271b69578d70ba8d8e3ce4d05192e9673
Denartha10 <mahonniall2012@gmail.com>
2024-04-02 09:23:42 +0100

    new changes

--- databaseScripts/bike_api.py
+++ databaseScripts/bike_api.py
@@ -1,18 +1,15 @@
-import requests
-
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float
 from sqlalchemy.orm import sessionmaker, declarative_base
-
 from datetime import datetime, time
+import requests
 import pytz
 
-#find current time
-dublin_timezone = pytz.timezone('Europe/Dublin') 
-current_time_dublin = datetime.now(dublin_timezone)
+# Set up timezone for Dublin
+current_time_dublin = datetime.now(pytz.timezone('Europe/Dublin'))
 
 Base = declarative_base()
 
-#python objects to push to db with alchemy
+# Define Availability class for database model
 class Availability(Base):
     __tablename__ = 'availability'
     id = Column(Integer, primary_key=True)
@@ -28,6 +25,7 @@ class Availability(Base):
         self.available_stands = stands
         self.status = status
 
+# Define Stations class for database model
 class Stations(Base):
     __tablename__ = 'stations'
     id = Column(Integer, primary_key=True)
@@ -49,64 +47,102 @@ class Stations(Base):
         self.lat = lat
         self.lng = lng
 
-#retuns json object array of bike stations
+
+# Function to make API call and return JSON data
 def api_call():
     api_key = 'c3888c0fb1578a56ea9015668577aa754fcbecd6'
     contract = 'Dublin'
     api_request = requests.get(f'https://api.jcdecaux.com/vls/v1/stations?contract={contract}&apiKey={api_key}')
     if api_request.status_code not in range(200, 299):
         print(api_request.reason)
-        return
+        return []
     data_json = api_request.json()
     return data_json
 
-#parameters to connect to RDS db
+# Function to set up database connection parameters
 def engine_params():
-    user = 'admin'
-    password = 'kukfiv-zubsyd-1Pejpu'
-    host = 'comp30830.c38umsk2i6vi.eu-north-1.rds.amazonaws.com'
-    port = '3306'
-    db_name = 'bike_db'
+    user = 'root'
+    password = 'password'
+    host = 'localhost'
+    port = '4321'
+    db_name = 'comp30830'
     return f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db_name}'
 
-def push_to_db():
-    #create connection to db
+
+
+
+data = api_call()
+
+stations = [
+    Stations(
+        id = d['number'],
+        up = datetime.fromtimestamp(d['last_update']/1000),
+        name = d['address'], 
+        bikes = d['available_bikes'], 
+        stands = d['available_bike_stands'], 
+        status = d['status'], 
+        lat = d['position']['lat'], 
+        lng = d['position']['lng']
+    )
+    for d in data
+]
+
+
+availabilities = [
+    Availability(
+        id = d['number'], 
+        up = datetime.fromtimestamp(d['last_update']/1000), 
+        bikes = d['available_bikes'], 
+        stands = d['available_bike_stands'], 
+        status = d['status']
+    )
+    for d in data
+]
+
+# Function to push data to the database
+def push_to_db(stations: list[Stations], availabilities: list[Availability]):
+
+    # create an engine instanse and a session
     engine = create_engine(engine_params())
 
-    #session will allows us to push batches to db at once
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    data = api_call()
-    
-    #Delete current_av table so theres only ever one block of entries 
+    # create a session factory
+    session_factory = sessionmaker(bind=engine)
+
+    # start a session
+    session = session_factory()
+
+    # delete all the existing Stations from the stations table
     session.query(Stations).delete()
     
-    #Add new rows from api_call
-    for d in data:
-        new_station = Stations(id = d['number'], up = datetime.utcfromtimestamp(d['last_update']/1000), name = d['address'], bikes = d['available_bikes'], stands = d['available_bike_stands'], status = d['status'], lat = d['position']['lat'], lng = d['position']['lng'])
-        session.add(new_station)
-    #Dont have the program crash if theres a duplicate primary key in there.
     try:
+        # add all the new stations
+        session.add_all(stations)
         session.commit()
     except:
-        #change this to print error to file
-        pass
-    
-    for d in data:
-        new_av = Availability(id = d['number'], up = datetime.utcfromtimestamp(d['last_update']/1000), bikes = d['available_bikes'], stands = d['available_bike_stands'], status = d['status'])
-        session.add(new_av)
+        print("Failed to update stations")
+        return False
+
+
     try:
+        session.add_all(availabilities)
         session.commit()
     except:
-        #change this to print error to file
-        pass
+        print("Failed to update the availabilities table")
+        return False
 
     session.close()
+    return True
 
-#check if the stations are closed
+
+# Function to check if the stations are closed
 def is_closed(now):
     return time(0, 30) <= now <= time(5, 0)
 
-#if stations open run the whole program
+
+# Main execution
 if not is_closed(current_time_dublin.time()):
-    push_to_db()
+    data_push_successful = push_to_db(stations, availabilities)
+    if data_push_successful:
+        print("\nData pushed successfully\n")
+    else:
+        print("\nFailed to update table. Please check code\n")
---
+++ utilities/map_utilities.py
@@ -0,0 +1,36 @@
+def determinePinColors(station): 
+    status = station["status"]
+    bike_count = station["available_bikes"]
+
+    if status != "OPEN":
+        return { 'background': "rgba(077, 077, 077)", 'border': "rgba(041, 041, 041)" }
+    elif bike_count > 5:
+        return { 'background': "rgba(046, 209, 138)", 'border': "rgba(019, 115, 051)" }
+    elif bike_count > 2:
+        return { 'background': "rgba(255, 255, 000)", 'border': "rgba(179, 179, 000)" }
+    elif bike_count > 0:
+        return { 'background': "rgba(255, 153, 000)", 'border': "rgba(153, 092, 000)" }
+    else:
+        return { 'background': "rgba(217, 098, 099)", 'border': "rgba(110, 023, 024)" }
+
+
+
+
+
+def createInfoWindowContent(station):
+    available_bikes = station["available_bikes"]
+    available_stands = station["available_stands"]
+    name = station["name"]
+    window_html = f"""
+        <div class="max-w-sm"> <!-- Increased the max width for a larger box -->
+            <div class="bg-white py-4 px-6 rounded-lg shadow-lg"> <!-- Increased padding for a larger box -->
+                <h1 class="text-xl font-semibold mb-4 text-center">{name}</h1> <!-- Made the name bigger, centered, and increased bottom margin -->
+                <div class="flex flex-col space-y-2"> <!-- Increased space between items -->
+                    <div><span class="font-semibold">Bikes Available:</span> <span class="{"text-red-600" if available_bikes == 0 else "text-gray-600"}">{available_bikes}</span></div>
+                    <div><span class="font-semibold">Stands Available:</span> <span class="text-gray-600">{available_stands}</span></div>
+                </div>
+            </div>
+        </div>
+    """
+
+    return window_html
