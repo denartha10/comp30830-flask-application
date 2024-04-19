45b0b80a5109ab90491dc72cb0db66e4b813da90
Adam Southwick <adamsouthwick@dhcp-892b1847.ucd.ie>
2024-04-19 12:32:13 +0100

    moved weather over to an api

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, Response, abort, render_template, request, send_from_directory
 from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
-from utilities.models import Station, Weather
+import requests
+import openmeteo_requests
 from utilities.map_utilities import determinePinColors, createInfoWindowContent
 import pandas as pd
 from dotenv import load_dotenv
@@ -39,21 +40,47 @@ def index():
 @app.route('/stations', methods=['GET'])
 @cache.cached()
 def get_stations():
-    stations = db.session.query(Station).all()
-    if not stations:
-        abort(404)  # error handler
-    bike_data = pd.DataFrame(stations)
+    api_key = 'c3888c0fb1578a56ea9015668577aa754fcbecd6'
+    contract = 'Dublin'
+    api_request = requests.get(f'https://api.jcdecaux.com/vls/v1/stations?contract={contract}&apiKey={api_key}')
+    if api_request.status_code not in range(200, 299):
+        print(api_request.reason)
+        return []
+    data_json = api_request.json()
+    df = pd.DataFrame(data_json)
+    bike_data = df[['number', 'address', 'position', 'available_bikes', 'available_bike_stands', 'status']]
+    
+    bike_data['lat'] = bike_data['position'].apply(lambda x: x['lat'])
+    bike_data['lng'] = bike_data['position'].apply(lambda x: x['lng'])
+    del bike_data['position']
+    
+    print(bike_data["lat"])
+    new_column_names = {
+    'number': 'id',
+    'address': 'name',
+    'available_bike_stands': 'available_stands',
+    'status': 'status',
+    }
+    bike_data = bike_data.rename(columns=new_column_names)
+    
     bike_data["pin_colors"] = bike_data.apply(determinePinColors, axis=1)
     bike_data["info_html"] = bike_data.apply(createInfoWindowContent, axis=1)
+    
     json_data = bike_data.to_json(orient='records', date_format='iso')
     return Response(json_data, mimetype='application/json')
 
 @app.route('/weather', methods=['GET'])
 def get_weather():
-    weather = db.session.query(Weather).all()
-    if not weather:
-        abort(404) # error handler
-    weather_data = pd.DataFrame(weather)
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+		"latitude": 53.35, #Dublin lat
+		"longitude": 6.26, #Dublin lng
+		"current": ["temperature_2m", "precipitation", "wind_speed_10m"]
+	}
+    openmeteo = openmeteo_requests.Client()
+    api_request = openmeteo.weather_api(url, params=params)
+    api_return = api_request[0].Current()
+    weather_data = pd.DataFrame(api_return)
     json_data = weather_data.to_json(orient='records', date_format='iso')
     return Response(json_data, mimetype='application/json')
 
