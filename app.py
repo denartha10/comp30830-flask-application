183516d6db077464ad3311a5d8b52d5b3853503f
Adam Southwick <adamsouthwick@dhcp-892b1847.ucd.ie>
2024-04-19 14:51:23 +0100

    cleaned up code

--- app.py
+++ app.py
@@ -25,10 +25,12 @@ cache = Cache(app)
 
 stations_dict = {}
 
+
 @app.route('/')
 def index():
     visited = request.cookies.get("visited") if request.cookies.get("visited") else "false"
     return render_template("index.html", visited=visited)
+
 
 @app.route('/stations', methods=['GET'])
 def get_stations():
@@ -60,6 +62,7 @@ def get_stations():
     response.set_cookie("visited", "true")
     return response
 
+
 @app.route('/weather', methods=['GET'])
 def get_weather():
     url = "https://api.open-meteo.com/v1/forecast"
@@ -83,14 +86,12 @@ def get_weather():
         
     return Response(weather, mimetype='application/json')
 
-@app.route('/select/id', methods=['GET'])
-def select_station(id):
-    select_station = db.session.query.filter_by(id=id).all() # Getting a warning about the filter by here??
-    if select_station is None:
-        abort(404) 
-    select_station_data = pd.DataFrame(select_station)
-    json_data = select_station_data.to_json(orient='records', date_format='iso')
-    return Response(json_data, mimetype='application/json')
+
+@app.route('/select_station')
+def serve_bike_avg_json():
+    bike_data_dir = os.path.join(app.root_path, 'bike_data')
+    return send_from_directory(bike_data_dir, 'bike_avg.json')
+
 
 @app.route('/predict', methods=['GET'])
 def get_prediction():
@@ -115,15 +116,12 @@ def get_prediction():
              abort(404)
     else:
          abort(404) 
-         
-@app.route('/bike_data/bike_avg.json')
-def serve_bike_avg_json():
-    bike_data_dir = os.path.join(app.root_path, 'bike_data')
-    return send_from_directory(bike_data_dir, 'bike_avg.json')
+    
     
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(port=5000)
